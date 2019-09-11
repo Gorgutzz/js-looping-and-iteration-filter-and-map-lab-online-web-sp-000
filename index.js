e01710fcefb770a2d94f612caa3a7769f82c288a
Leo Kalshteyn <leo.kalshteyn@gmail.com>
2019-09-11 22:23:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution here:
+function driversWithRevenueOver(drivers) {
+
+  return driverlist.map(function(driver) {
+
+    return driver.toLowerCase();
+  });
+}
+
+function nameToAttributes(driverlist) {
+
+  return(driverlist.map(function(driver) {
+
+    const firstname = driver.split(' ')[0];
+    const lastname = driver.split(' ')[1];
+
+    return({ firstName: firstname, lastName: lastname }); }
+  ));
+}
+
+function attributesToPhrase(driverlist) {
+
+  return(driverlist.map(function(driver) {
+
+    return(`${driver.name} is from ${driver.hometown}`); }
+  ));
+}
